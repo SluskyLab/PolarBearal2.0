0836473f8c9a60290ef5e60d254ce61a82a61139
Rik Dhar <rikdhar@gmail.com>
2020-12-14 12:48:43 -0600

    Removing files in DB and Output

--- Output/all/Pymol/strands/strands_3J9C.py
+++
@@ -1,7 +0,0 @@
-from pymol import cmd, stored
-x = r"C:\Users\rikdh\Dropbox (Slusky Lab)\Rik\Projects_Rik\Barrel Comparison\Git_polar_bearal/DB/PDBs/3J9C.pdb"
-cmd.load(x)
-cmd.hide("everything", "all")
-cmd.color("wheat","all")
-cmd.select("barrel", "cmd.show("cartoon", "barrel")
-cmd.zoom("barrel")
